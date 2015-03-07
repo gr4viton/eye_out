c0ca69fa78f6cc4d0afb21a0893db08592a9da4c
gr4viton <lordmutty@gmail.com>
2015-03-07 21:26:56 +0100

    wpf base integrated - not functions..

---
+++ EyeOut/EyeOut/EyeOut/C_controlMot.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace EyeOut
+{
+    public class C_controlMot
+    {
+        public static string baudRate_str;
+        private int baudRate_val;
+
+
+        //raises exception
+        public string _BaudRate
+        {
+            get { return baudRate_str; }
+            set
+            {
+                baudRate_str = value;
+                //this.baudRate_str = value;
+                /*
+                if (!this.CHECK_level_and_numOfTickets(this.level, value))
+                {
+                    throw new ApplicationException(
+                        "Máš moc malej level na to abys mohl mít tolik vstupenek!");
+                }
+                if (this.numOfTickets <= 0)
+                {
+                    throw new ApplicationException(
+                        "Kup si něco na sebe! aspoň jednu vstupenku.. vogo");
+                }*/
+            }
+        }
+    }
+}
---
+++ EyeOut/EyeOut/EyeOut/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EyeOut
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            int[] brs = { 9600, 57600, 1000000 };
+            foreach (int br in brs)
+            {
+                lsBaudRate.Items.Add(Convert.ToString(br));
+            }
+            lsBaudRate.SelectedIndex = 1;
+        }
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            txMotLog.Text = C_controlMot.baudRate_str;
+            //txMotLog.Text = selectedItem.Content.ToString();
+        }
+
+        private ListBoxItem selectedItem;
+        public ListBoxItem SelectedItem
+        {
+            get
+            {
+                return selectedItem;
+            }
+            set
+            {
+                selectedItem = value;
+                /*
+                RaisePropertyChanged("SelectedItem");
+
+                var thePanel = new StackPanel();
+                thePanel = ((((((selectedItem as ListBoxItem).Parent as ListBox).Parent as StackPanel).Parent as Grid).Parent as StackPanel).Parent as MainWindow).thePanel;
+                string message;
+                message = selectedItem.ToString();
+
+                if (message == "Data entries")
+                {
+                    var allEntries = new CategoryViewModel();
+
+                    foreach (var category in (thePanel.DataContext as UserViewModel).Categories)
+                        allEntries.Entries = new ObservableCollection<EntryViewModel>(category.Entries);
+
+                    thePanel.Children.Clear();
+                    thePanel.Children.Add(new EntriesUC(allEntries));
+                }
+                // implementation for all the other list items...
+                 * */
+            }
+        }
+
+    }
+}
