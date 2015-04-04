8ff0e9c99777584fbe60a041ddf5c165bb34c81e
gr4viton <lordmutty@gmail.com>
2015-04-05 01:55:52 +0200

    still not working binding..

--- EyeOut/EyeOut/EyeOut/mot/C_MotorDataRow.cs
+++ EyeOut/EyeOut/EyeOut/mot/C_MotorDataRow.cs
@@ -27,54 +27,46 @@ namespace EyeOut
         public e_motorDataType type { get; set; }
         public string name { get; set; }
 
+        public static event EventHandler yawChanged;
+        //public static event EventHandler pitchChanged;
+        //public static event EventHandler rollChanged;
+
         public string yaw
         {
             get { return GET_ypr(e_rot.yaw); }
-            set { yaw = value; }
-            //set { SET_ypr(e_rot.yaw, value); }
+            set
+            {
+
+                //SET_ypr(e_rot.yaw, value);
+                EventHandler handler = yawChanged;
+                if (handler != null)
+                    handler(null, EventArgs.Empty);
+            }
         }
         public string pitch
         {
             get { return GET_ypr(e_rot.pitch); }
-            set { pitch = value; }
-            //set { SET_ypr(e_rot.pitch, value); }
+            set { SET_ypr(e_rot.pitch, value); }
         }
         public string roll
         {
             get { return GET_ypr(e_rot.roll); }
-            set { roll = value; }
-            //set { SET_ypr(e_rot.roll, value); }
+            set { SET_ypr(e_rot.roll, value); }
         }
-        //private string[] ypr;
 
-        /*
-        public void REFRESH_ypr(e_rot rot)
-        {
-            switch (rot)
-            {
-                case (e_rot.yaw): SET_ypr(rot, yaw); break;
-                case (e_rot.pitch): SET_ypr(rot, pitch); break;
-                case (e_rot.roll): SET_ypr(rot, roll); break;
-            }
-        }*/
+        private string[] ypr;
+
         public string GET_ypr(e_rot rot)
         {
-            switch (rot)
-            {
-                case (e_rot.yaw): return yaw; break;
-                case (e_rot.pitch): return pitch; break;
-                case (e_rot.roll): return roll; break;
-            }
-            return null;
+            return ypr[(int)rot];
         }
-        
+
         public void SET_ypr(e_rot rot, string value)
         {
+            ypr[(int)rot] = value;
             switch (rot)
             {
                 case (e_rot.yaw): yaw = value; break;
-                case (e_rot.pitch): pitch = value; break;
-                case (e_rot.roll): roll = value; break;
             }
         }
         //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -82,6 +74,7 @@ namespace EyeOut
         {
             type = _type;
             name = EnumGetDescription.GetDescription((e_motorDataType)type);
+            ypr = new string[3];
             REFRESH();
         }
 
